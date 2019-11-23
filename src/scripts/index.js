2a5babbf707a71385b69cb2dc2fb4e98fc61f752
echowtyang <echowtyang@tencent.com>
2019-11-23 14:38:59 +0800

    update

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,10 @@
 import '../styles/normalize.scss';
 import '../styles/style.scss';
 require('../styles/mystyles.scss');
+
+const $ = require("jquery");
+console.log(123);
+console.log($);
+const showModal = ( _type ) => {
+  console.log(_type);
+};
